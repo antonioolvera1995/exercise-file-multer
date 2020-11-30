336c3e2ab2653252900585a53cee1fd3aeb8a172
PC-ANTONIO\toni_ <antonioolvera1995@gmail.com>
2020-11-30 12:09:37 +0100

    *v1*

--- src/controllers/index.controllers.ts
+++ src/controllers/index.controllers.ts
@@ -8,34 +8,35 @@ class IndexController {
     storage = multer.diskStorage({
 
         destination: (req, file, cb) => {
-            cb(null, path.join(__dirname, './../uploads/'));
+
+            cb(null, path.join(__dirname, `./../uploads/avatar${req.params.id}`));
         },
 
         filename: (req, file, cb) => {
+
+            var ext = path.extname(file.originalname);
+            if (ext !== '.png' && ext !== '.jpg' && ext !== '.jpeg') {
+                throw new Error('Only images are allowed');
+            }
+
+            file.originalname = `User${ext}`;
             cb(null, file.originalname);
         }
 
     });
-
     upload = multer({ storage: this.storage });
 
 
 
     async index(req: Request, res: Response) {
 
-
-
         try {
             
-
             res.send(req.file);
-
 
         } catch (error) {
             res.send(error);
         }
-
-
 
     }
 
--- src/routes/index.routes.ts
+++ src/routes/index.routes.ts
@@ -1,39 +1,19 @@
-import {Router} from 'express';
+import { Router } from 'express';
 import { indexController } from '../controllers/index.controllers';
-import multer from 'multer';
-import path from 'path';
+
 
 class IndexRouter {
 
-    router:Router = Router();
+    router: Router = Router();
 
-    
-    storage = multer.diskStorage({
 
-        destination: (req, file, cb) => {
-         
-            cb(null, path.join(__dirname, `./../uploads/avatar${req.params.id}`));
-        },
-
-        filename: (req, file, cb) => {
-            
-            var ext = path.extname(file.originalname);
-            if(ext !== '.png' && ext !== '.jpg' && ext !== '.jpeg') {
-                throw new Error('Only images are allowed');
-            }
-
-            cb(null, file.originalname);
-        }
-
-    });
-
-    upload = multer({ storage: this.storage });
+   
 
     constructor() {
 
-        this.router.post('/users/:id/avatar', this.upload.single('file'), indexController.index);
+        this.router.post('/users/:id/avatar', indexController.upload.single('file'), indexController.index);
 
-        
+
     }
 
 
